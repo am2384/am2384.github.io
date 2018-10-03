fa4e45e8ee6803d9e87700dc00dbf2a9582051ad
Alex Li <afl59@cornell.edu>
2018-10-03 17:04:57 -0400

    detectIR hat and PID

--- code/detectIRHat/detectIRHat.ino
+++ code/detectIRHat/detectIRHat.ino
@@ -11,6 +11,19 @@ port at 115.2kb.
 #define FFT_N 256 // set to 256 point fft
 
 #include <FFT.h> // include the library
+#include <Servo.h>
+
+Servo servo0;  // create servo object for the left servo
+Servo servo1;  // create servo object for the right servo
+int lineSensors[5] = {0, 0, 0, 0, 0};
+int pos;
+int proportional;
+int derivative;
+int last_proportional;
+int integral = 0;
+int k1 = 20;
+int k2 = 1;
+int k3 = 1;
 
 void setup() {
   Serial.begin(115200); // use the serial port
@@ -18,13 +31,30 @@ void setup() {
   ADCSRA = 0xe5; // set the adc to free running mode
   ADMUX = 0x40; // use adc0
   DIDR0 = 0x01; // turn off the digital input for adc0
+
+  // from Sheila
+  servo0.attach(3,1300,1700);  // attaches the servo on pin 5 to the servo object
+  servo1.attach(5,1300,1700);  // attaches the servo on pin 6 to the servo object
+  pinMode(9, INPUT);
+  pinMode(10, INPUT);
+  pinMode(11, INPUT);
+  pinMode(12, INPUT);
+  pinMode(13, INPUT);
 }
 
 void loop() {
+  readLineSensors();
   int result = readIR();
   if(result == 1){
     Serial.println("IR Hat!");
   }
+  delay(100);
+}
+
+void runServo(int leftSpeed, int rightSpeed)
+{
+  servo0.write(90+leftSpeed); 
+  servo1.write(90-rightSpeed);
 }
 
 int readIR(){
@@ -51,5 +81,61 @@ int readIR(){
     else{
       return 0;
     }
+}
+
+void readLineSensors()
+{
+  lineSensors[0] = !(digitalRead(9));
+  lineSensors[1] = !(digitalRead(10));
+  lineSensors[2] = !(digitalRead(11));
+  lineSensors[3] = !(digitalRead(12));
+  lineSensors[4] = !(digitalRead(13));
+  
+  if(lineSensors[0]) pos = 2;
+  else if(lineSensors[1]) pos = 1;
+  else if(lineSensors[2]) pos = 0;
+  else if(lineSensors[3]) pos = -1;
+  else if(lineSensors[4]) pos = -2;
+
+  Serial.println(pos);
+  int power_difference = pos*k1;
+  if(power_difference > 0)
+      runServo(90-power_difference, 90);
+  else
+      runServo(90, 90-power_difference);
+}
+
+void PID()
+{
+  proportional = pos;
+  derivative = proportional - last_proportional;
+  integral = integral + proportional;
+  last_proportional = proportional;
+
+  float power_difference = proportional * k1 + integral * k2 + derivative * k3;
+
+  // Compute the motor settings. The motor's accepts values between 0 and 180. 
+
+  if(power_difference > 90)
+     power_difference = 90;
+  if(power_difference < -90)
+     power_difference = -90;
+
+  // set the motors to the correct speed
+  if(power_difference < 0)
+      runServo(90-power_difference, 90);
+  else
+      runServo(90, 90-power_difference);
+}
+
+void stopServos()
+{
+  runServo(0, 0);
+}
+
+// Go straight
+void goStraight()
+{
+   runServo(90, 90);
 }
 
