8ffc2edf1636c09210b74ee473c536b9ab371e43
sheilabalu <balu.sheila@gmail.com>
2018-10-20 18:30:43 -0400

    Lab 3 minus radio
    location tracking

--- code/PID3/PID3.ino
+++ code/PID3/PID3.ino
@@ -23,38 +23,58 @@ int sensorValueRight;
 int sensorPinFront = A3;
 int sensorValueFront;
 int start = 0;
+int dir = 0;
+int x = 0;
+int y = 0;
+
+typedef union {
+  unsigned c: 8;
+  struct {
+    unsigned t_color: 1;
+    unsigned t_shape: 2;
+    unsigned robot: 1;
+    unsigned west: 1;
+    unsigned east: 1;
+    unsigned south: 1;
+    unsigned north: 1;
+  };
+} square_data_t;
+
+square_data_t maze_data[9][9];
+square_data_t c_square;
+
+int self_x = 0, self_y = 0;
 
 void setup() {
   //Serial.begin(115200);
   Serial.begin(9600);
   servo0.attach(3,1300, 1700);  // attaches the servo on pin 5 to the servo object
   servo1.attach(5,1300, 1700);  // attaches the servo on pin 6 to the servo object
-  pinMode(9, INPUT);
-  pinMode(10, INPUT);
-  pinMode(11, INPUT);
-  pinMode(12, INPUT);
-  pinMode(13, INPUT);
-  pinMode(7, OUTPUT);
+  pinMode(6, INPUT);
+  pinMode(8, INPUT);
+  pinMode(2, INPUT);
+  pinMode(4, INPUT);
+  pinMode(7, INPUT);
 }
 
 void loop() {
-  if(start == 0)
-  {
-    while(audio() == 0)
-    {
-      Serial.println("No tone");
-    }
-    start = 1;
-  }
+//  if(start == 0)
+//  {
+//    while(audio() == 0)
+//    {
+//      Serial.println("No tone");
+//    }
+//    start = 1;
+//  }
   
   if(readIR()==1)
   {
     Serial.println("IR Hat Detected");
-    //make180turn();
+    make180turn();
   } 
   else
   {
-    Serial.println("");
+    //Serial.println("");
   }
 
   if (!checkIntersection()) // we are not at an intersection
@@ -63,32 +83,90 @@ void loop() {
   } 
   else // we are at an intersection
   {
-    Serial.println("Intersection\n");
+    Serial.println("Intersection");
     if(rightSensor() == 0)
     { 
-      turnRightSweep();
-    } else if(frontSensor() == 1) 
+    turnRightSweep();
+      if(dir == 0) y++;
+      if(dir == 1) x++;
+      if(dir == 2) y--;
+      if(dir == 3) x--;
+      if(dir == 0) c_square.east = 0;
+      else if (dir == 1) c_square.south = 0;
+      else if (dir == 2) c_square.west = 0;
+      else c_square.north = 0;
+      dir++;      
+      if(dir == 4) dir = 0;
+    } 
+    else if(frontSensor() == 1) 
     {
       turnLeftSweep();
+      if(dir == 0) y++;
+      if(dir == 1) x++;
+      if(dir == 2) y--;
+      if(dir == 3) x--;
+      if(dir == 0){
+        c_square.east = 1;
+        c_square.north = 1;
+      }
+      else if (dir == 1){
+        c_square.south = 1;
+        c_square.east = 1;
+      }
+      else if (dir == 2){
+        c_square.west = 1;
+        c_square.south = 1;
+      }
+      else{
+        c_square.north = 1;
+        c_square.west = 1;
+      }
+      dir--;
+      if(dir < 0) dir = 3;
     }
     else
     {
       PIDControl();
+      if(dir == 0) y++;
+      if(dir == 1) x++;
+      if(dir == 2) y--;
+      if(dir == 3) x--;
+      if(dir == 0) c_square.east = 1;
+      else if (dir == 1) c_square.south = 1;
+      else if (dir == 2) c_square.west = 1;
+      else c_square.north = 1;
     }
+    
+    Serial.print(" North:");
+    Serial.print(c_square.north);
+    Serial.print(" East:");
+    Serial.print(c_square.east);
+    Serial.print(" South:");
+    Serial.print(c_square.south);
+    Serial.print(" West:");
+    Serial.print(c_square.west);
+    Serial.print(" Dir:");
+    Serial.print(dir);
+    Serial.print(" X:");
+    Serial.print(x);
+    Serial.print(" Y:");
+    Serial.print(y);
+    Serial.print(" Dir:");
+    Serial.println(dir);
   }
 }
 
 int frontSensor()
 {
     sensorValueFront = analogRead(sensorPinFront);
-  if(sensorValueFront<200) return 0;
+  if(sensorValueFront<300) return 0;
   else return 1;
 }
 
 int rightSensor()
 {
   sensorValueRight = analogRead(sensorPinRight);
-  if(sensorValueRight<200) return 0;
+  if(sensorValueRight<300) return 0;
   else return 1;
 }
 
@@ -190,12 +268,10 @@ int audio()
 
 void turnRightSweep()
 {
-  digitalWrite(7, HIGH);
   runServo(50, -30);
   delay(900);
   runServo(10, 10);
   delay(400);
-  digitalWrite(7, LOW);
 }
 
 void mydelay(int count)
@@ -207,12 +283,10 @@ void mydelay(int count)
 }
 
 void turnLeftSweep() {
-  digitalWrite(7, HIGH);
   runServo(-30, 50);
   delay(900);
   runServo(10, 10);
   delay(400);
-  digitalWrite(7, LOW);
 }
 
 void runServo(int leftSpeed, int rightSpeed)
@@ -225,27 +299,27 @@ void runServo(int leftSpeed, int rightSpeed)
 
 bool checkIntersection() 
 {
-  line[0] = !(digitalRead(9));
-  line[1] = !(digitalRead(10));
-  line[2] = !(digitalRead(11));
-  line[3] = !(digitalRead(12));
-  line[4] = !(digitalRead(13));
-  /*Serial.println(line[0]);
-  Serial.println(line[1]);
-  Serial.println(line[2]);
-  Serial.println(line[3]);
-  Serial.println(line[4]);
+  line[0] = !(digitalRead(6));
+  line[1] = !(digitalRead(8));
+  line[2] = !(digitalRead(2));
+  line[3] = !(digitalRead(4));
+  line[4] = !(digitalRead(7));
+  /*Serial.print(line[0]);
+  Serial.print(line[1]);
+  Serial.print(line[2]);
+  Serial.print(line[3]);
+  Serial.print(line[4]);
   Serial.println("");*/
   return (line[0] && line[1] && line[2] && line[3] && line[4]);
 }
 
 int IRmeasurements()
 {
-  line[0] = !(digitalRead(9));
-  line[1] = !(digitalRead(10));
-  line[2] = !(digitalRead(11));
-  line[3] = !(digitalRead(12));
-  line[4] = !(digitalRead(13));
+  line[0] = !(digitalRead(6));
+  line[1] = !(digitalRead(8));
+  line[2] = !(digitalRead(2));
+  line[3] = !(digitalRead(4));
+  line[4] = !(digitalRead(7));
   //    0   1   2   3   4 
   if(line[2] == 1 && line[0] == 0 && line[1] == 0 && line[3] == 0 && line[4] == 0)  ///////////////// 2 only straight 
   {
